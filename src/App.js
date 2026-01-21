f882457c18411192bed4c8fdb495f35d76def092
AnushaAkkena <120028427+AnushaAkkena@users.noreply.github.com>
2026-01-21 21:18:05 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>🚀 AWS Static Website PoC</h1>
+      <p>Deployed using CodeBuild, Private S3 and CloudFront (OAC)</p>
+
+      <div className="card">
+        <h3>Pipeline</h3>
+        <ul>
+          <li>GitHub</li>
+          <li>CodePipeline</li>
+          <li>CodeBuild</li>
+          <li>CloudFront</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
