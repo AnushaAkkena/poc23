0cef1d3fd77438a777ab349f1fb7ada3eaf67143
AnushaAkkena <120028427+AnushaAkkena@users.noreply.github.com>
2026-01-22 20:40:48 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,14 +4,13 @@ import "./App.css";
 function App() {
   return (
     <div className="app">
-      <h1>🚀 AWS Static Website PoC</h1>
+      <h1> 🚀 AWS Static Website PoC 🚀 </h1>
       <p>Deployed using CodeBuild, Private S3 and CloudFront (OAC)</p>
 
       <div className="card">
         <h3>Pipeline</h3>
         <ul>
           <li>GitHub</li>
-          <li>CodePipeline</li>
           <li>CodeBuild</li>
           <li>CloudFront</li>
         </ul>
